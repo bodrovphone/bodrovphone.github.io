706b6a84bbd62cc8958ba0dd59f8cac360813796
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2019-05-28 15:58:01 +0300

    whitelist domain test

---
+++ initializr/js/ui.js
@@ -0,0 +1,6 @@
+$BV.ui( 'rr', 'show_reviews', {
+  doShowContent : function () { 
+    // If the container is hidden (such as behind a tab), put code here to make it visible 
+    //(open the tab).
+  }
+});
